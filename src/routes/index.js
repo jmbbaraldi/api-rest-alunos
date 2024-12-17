aac9b1acbc6f2626552461415319700783edee22
João Baraldi <jmbbaraldi@gmail.com>
2024-12-17 17:03:51 -0300

    jwt ready

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import homeRoutes from './homeRoutes';
 import userRoutes from './userRoutes';
+import tokenRoutes from './tokenRoutes';
 
 function Routes(app) {
   app.use('/', homeRoutes);
   app.use('/users/', userRoutes);
+  app.use('/tokens/', tokenRoutes);
 }
 
 export default Routes;
